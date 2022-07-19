1bb4735842f0f60868afa922ca9c56fec5bd8978
marlonoloo <95957350+marlonoloo@users.noreply.github.com>
2022-07-19 16:18:33 +0300

    Initialized project

---
+++ BenefitsBackendApp/src/main/java/com/emtech/BenefitsBackendApp/BenefitsBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.emtech.BenefitsBackendApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenefitsBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenefitsBackendAppApplication.class, args);
+	}
+
+}
---
+++ BenefitsBackendApp/src/test/java/com/emtech/BenefitsBackendApp/BenefitsBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emtech.BenefitsBackendApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenefitsBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
